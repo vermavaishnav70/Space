48cfff6837d5e7e72fa06674a8313b8acd6ea571
Vaishnav Verma <vermavaishnav70@gmail.com>
2024-09-18 21:24:40 +0530

    Initial commit

---
+++ app/landing/_components/third-section.jsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import React from 'react';
+
+import { Lora } from 'next/font/google';
+import { cn } from '@/lib/utils';
+
+
+const font = Lora({
+    subsets: ["latin"],
+    weight: ["400"],
+})
+
+const ThirdSection = () => {
+    return (
+      <div className="xl:pt-32 pt-24 relative flex justify-center items-center flex-col text-6xl text-center">
+        New age of collaboration
+        <div className="xl:text-5xl text-3xl 2xl:w-3/5 w-3/5 font-medium xl:w-1/3 mx-auto text-center">
+          Cut costs
+        </div>
+        <Image
+          src="/assets/canva-logo.png"
+          alt="Canva logo"
+          width={1000}
+          height={1000}
+          className="pt-10 xl:pt-10 
+                xl:w-1/3
+                w-4/5
+                scale-150
+                "
+        />
+        <div
+          className={cn(
+            "flex items-center justify-center text-xl xl:text-2xl pt-10 pb-4  xl:py-10 px-8  text-center  w-4/5 ",
+            font.className
+          )}
+        >
+          &quot;Get rid of nearly a dozen different tools because of what
+          Space will do for you.&quot;
+        </div>
+      </div>
+    );
+}
+
+export default ThirdSection;
---
+++ app/landing/page.jsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/navbar/navbar";
+import HeroSection from "./_components/hero-section";
+import ThirdSection from "./_components/third-section";
+import FourthSection from "./_components/fourth-section";
+import FifthSection from "./_components/fifth-section";
+import SeventhSection from "./_components/seventh-section";
+import GetStartedFree from "./_components/eighth-section";
+import Footer from "@/components/footer";
+
+const LandingPage = () => {
+    return ( 
+    <div className="">
+        <Navbar />
+        <HeroSection />
+        <ThirdSection />
+        <FourthSection />
+        <FifthSection />
+        <SeventhSection />
+        <GetStartedFree />
+        <Footer />
+
+    </div> );
+}
+ 
+export default LandingPage;
