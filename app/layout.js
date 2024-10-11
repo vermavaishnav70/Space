3d9fbe763ebf955d9ce74b337fbc7fa50c01500c
Vaishnav Verma <vermavaishnav70@gmail.com>
2024-10-11 08:41:14 +0530

    add meta data

--- app/layout.js
+++ app/layout.js
@@ -16,14 +16,35 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Inject metadata for SEO purposes */}
+        <meta
+          name="description"
+          content="Space is the connected workspace where better, faster work happens."
+        />
+        <meta property="og:title" content="Space" />
+        <meta
+          property="og:description"
+          content="Space is the connected workspace where better, faster work happens."
+        />
+        <meta property="og:url" content="https://space-qnih.vercel.app" />
+        <meta property="og:type" content="website" />
+
+        {/* Google site verification */}
+        <meta
+          name="google-site-verification"
+          content="0W_TGLiCZhf1avSSbH2mBZQP1jTYQ2CZl6sTVPtNNBw"
+        />
+        <title>Space</title>
+      </head>
       <body className={inter.className}>
         <ConvexClientProvider>
           <EdgeStoreProvider>
-          <ThemeProvider defaultTheme="light" attribute="class">
-          <Toaster position="bottom-center" />
-          {children}
-          <ModalProvider />
-          </ThemeProvider>
+            <ThemeProvider defaultTheme="light" attribute="class">
+              <Toaster position="bottom-center" />
+              {children}
+              <ModalProvider />
+            </ThemeProvider>
           </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
